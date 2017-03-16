01e97b92d6b54a05892e528dfd7d11649acc8331
vbudi000 <vbudi@yahoo.com>
2017-03-16 14:59:29 -0500

    another fsrevert

--- fsrevert.sh
+++ fsrevert.sh
@@ -4,7 +4,13 @@
    git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative
    cd refarch-cloudnative
    git checkout 9d148ecb740d89bbcdfda1ae0e9cf02edf5bceba
-   ./clonePeers.sh
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-api
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-bluecompute-web
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-mysql
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-bff-socialreview
+   git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-bff-inventory
+
    cd ../refarch-cloudnative-api; git checkout 713ff236c7184f5a26987e27a6d0091ab1314ed9
    cd ../refarch-cloudnative-bluecompute-web; git checkout 8396d824665968e1040e423308b7e8e378f62280
    cd ../refarch-cloudnative-micro-inventory; git checkout 989257cb91dd9eb3571f798b4a7a7200a62682ee
